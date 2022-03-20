c328a03c4fe532ebabf459713185d8ab50592e50
Wang Pengfei <13671619364@sina.cn>
2022-03-21 07:55:40 +0800

    initialize repo

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import {LayoutPage} from "../typing";
+import {Box, HStack} from "@chakra-ui/react";
+
+const Home: LayoutPage = () => {
+    return (
+        <Box>
+            After Work
+        </Box>
+    );
+};
+
+export default Home;
